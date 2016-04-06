ee949331b7696024567ac440267a13b8be8d6564
eccentricdevotion <eccentricdevotion@gmail.com>
2016-04-07 09:19:58 +1200

    Update old configs with new lockable blocks

--- src/main/java/me/eccentric_nz/lockclock/LockClockConfig.java
+++ src/main/java/me/eccentric_nz/lockclock/LockClockConfig.java
@@ -1,7 +1,9 @@
 package me.eccentric_nz.lockclock;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -14,6 +16,7 @@ public class LockClockConfig {
 //    HashMap<String, String> strOptions = new HashMap<String, String>();
 //    HashMap<String, Integer> intOptions = new HashMap<String, Integer>();
     HashMap<String, Boolean> boolOptions = new HashMap<String, Boolean>();
+    private List<String> lockables = Arrays.asList("ACACIA_DOOR", "ACACIA_FENCE_GATE", "BIRCH_DOOR", "BIRCH_FENCE_GATE", "DARK_OAK_DOOR", "DARK_OAK_FENCE_GATE", "FENCE_GATE", "GOLD_PLATE", "IRON_PLATE", "IRON_TRAPDOOR", "JUNGLE_DOOR", "JUNGLE_FENCE_GATE", "SPRUCE_DOOR", "SPRUCE_FENCE_GATE");
 
     public LockClockConfig(LockClock plugin) {
         this.plugin = plugin;
@@ -32,6 +35,18 @@ public class LockClockConfig {
                 plugin.getConfig().set(entry.getKey(), entry.getValue());
                 i++;
             }
+        }
+        int a = 0;
+        List<String> tmp = config.getStringList("lockables");
+        for (String l : lockables) {
+            if (!config.getStringList("lockables").contains(l)) {
+                tmp.add(l);
+                a++;
+            }
+        }
+        if (a > 0) {
+            plugin.getConfig().set("lockables", tmp);
+            System.out.println("[LockClock] Added " + a + " new lockable blocks to config");
         }
 //        // int values
 //        for (Map.Entry<String, Integer> entry : intOptions.entrySet()) {
